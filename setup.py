7fb62e7b246ff2374ed44a96f440364c6c5a2a4d
Phil Daly <pndaly@email.arizona.edu>
2018-06-14 08:03:03 -0700

    New setup.py after module integration

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 
 # install script for PlanteomeDeepSegment
 import sys
-from bq.setup.module_setup import python_setup
 from bq.setup.module_setup import read_config
 from bq.setup.module_setup import docker_setup
 
 
 def setup(i_params, *args, **kw):
     print('args={}, kw={}'.format(args, kw))
-    # python_setup('PlanteomeDeepSegment.py', params=i_params)
     docker_setup('bisque_uplanteome', 'PlanteomeDeepSegment', 'planteomedeepsegment', params=i_params)
 
 
